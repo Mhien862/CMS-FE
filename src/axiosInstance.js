7326c4fb5dbe507c1a6b5eb17b0d7054100c666d
Mhien862 <104289231+Mhien862@users.noreply.github.com>
2024-10-23 11:24:11 +0700

    Refactor build script to increase memory limit

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:3000',
-    baseURL: 'https://cms-backend-5.onrender.com',
+   baseURL: 'https://cms-backend-5.onrender.com', 
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
